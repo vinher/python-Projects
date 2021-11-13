f38cf002f41a205ea51c2d736cefb565b41ef16a
unknown <vinher@gmail.com>
2021-11-13 11:09:49 -0600

    Juego del gato listo

--- 12.-Juego-Del-Gato.py
+++ 12.-Juego-Del-Gato.py
@@ -4,33 +4,34 @@ import time
 
 tablero = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-def estructura_tablero(tablero):
+def dibujar_tablero(tablero):
     print(" %c | %c | %c " % (tablero[0],tablero[1],tablero[2]))
     print("---+---+---")
     print(" %c | %c | %c " % (tablero[3],tablero[4],tablero[5]))
     print("---+---+---")
     print(" %c | %c | %c " % (tablero[6],tablero[7],tablero[8]))
 
-def estado_juego(tablero):
+def estado_del_juego(tablero):
     #Lineas horizontales
-    if(tablero[0] == tablero == [1] == tablero[2] !=' '):
+    if(tablero[0] == tablero[1] == tablero[2] !=' '):
         estado_actual = "Ganador"
     elif(tablero[3]==tablero[4]==tablero[5] != ' '):
         estado_actual = "Ganador"
     elif(tablero[6] == tablero[7] == tablero[8] != ' '):
         estado_actual = "Ganador"
     #Líneas verticales
-    elif(tablero[0] == tablero == [3] == tablero[6] !=' '):
+    elif(tablero[0] == tablero [3] == tablero[6] !=' '):
         estado_actual = "Ganador"
-    elif(tablero[1] == tablero == [4] == tablero[7] !=' '):
+    elif(tablero[1] == tablero[4] == tablero[7] !=' '):
         estado_actual = "Ganador"
-    elif(tablero[2] == tablero == [5] == tablero[6] !=' '):
+    elif(tablero[2] == tablero[5] == tablero[6] !=' '):
         estado_actual = "Ganador"
     #Líneas Diagonales
-    elif(tablero[0] == tablero == [4] == tablero[8] !=' '):
+    elif(tablero[0] == tablero[4] == tablero[8] != ' '):
         estado_actual = "Ganador"
-    elif(tablero[6] == tablero == [4] == tablero[2] !=' '):
+    elif(tablero[6] == tablero[4] == tablero[2] != ' '):
         estado_actual = "Ganador"
+
     else:
         estado_actual = "Jugando"
 
@@ -39,7 +40,9 @@ def estado_juego(tablero):
 
 #Constantes iniciales
 jugador_actual = "X"
+jugador_actual = "X"
 estado_actual = "Jugando"
+
 turno = 1
 print("Estructura Del Tablero: ")
 print(" 1 | 2 | 3 ")
@@ -66,9 +69,9 @@ while(True):
         print("Posicion no valida")
         continue
 
-    estructura_tablero(tablero)
+    dibujar_tablero(tablero)
 
-    estado_actual = estado_juego(tablero)
+    estado_actual = estado_del_juego(tablero)
 
     if estado_actual == "Jugando":
 
